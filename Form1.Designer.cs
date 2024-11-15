14cb958de95c83a86f2e8eee5e6ed01327afb4c7
ark-l <ark-l@ukr.net>
2024-11-16 01:38:42 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgMatrix = new System.Windows.Forms.DataGridView();
+            this.tbSize = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnMatrix = new System.Windows.Forms.Button();
+            this.btnLU = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnU = new System.Windows.Forms.Button();
+            this.btnL = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgMatrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgMatrix
+            // 
+            this.dgMatrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgMatrix.Location = new System.Drawing.Point(25, 12);
+            this.dgMatrix.Name = "dgMatrix";
+            this.dgMatrix.RowHeadersWidth = 51;
+            this.dgMatrix.RowTemplate.Height = 24;
+            this.dgMatrix.Size = new System.Drawing.Size(776, 262);
+            this.dgMatrix.TabIndex = 0;
+            // 
+            // tbSize
+            // 
+            this.tbSize.Location = new System.Drawing.Point(851, 23);
+            this.tbSize.Name = "tbSize";
+            this.tbSize.Size = new System.Drawing.Size(100, 22);
+            this.tbSize.TabIndex = 1;
+            this.tbSize.Text = "4";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(921, 64);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(84, 30);
+            this.btnTest.TabIndex = 2;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // btnMatrix
+            // 
+            this.btnMatrix.Location = new System.Drawing.Point(822, 64);
+            this.btnMatrix.Name = "btnMatrix";
+            this.btnMatrix.Size = new System.Drawing.Size(84, 30);
+            this.btnMatrix.TabIndex = 3;
+            this.btnMatrix.Text = "Create";
+            this.btnMatrix.UseVisualStyleBackColor = true;
+            this.btnMatrix.Click += new System.EventHandler(this.btnMatrix_Click);
+            // 
+            // btnLU
+            // 
+            this.btnLU.Location = new System.Drawing.Point(851, 191);
+            this.btnLU.Name = "btnLU";
+            this.btnLU.Size = new System.Drawing.Size(84, 30);
+            this.btnLU.TabIndex = 4;
+            this.btnLU.Text = "LU";
+            this.btnLU.UseVisualStyleBackColor = true;
+            this.btnLU.Click += new System.EventHandler(this.btnLU_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(994, 208);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(212, 276);
+            this.listBox1.TabIndex = 5;
+            // 
+            // btnU
+            // 
+            this.btnU.Location = new System.Drawing.Point(908, 244);
+            this.btnU.Name = "btnU";
+            this.btnU.Size = new System.Drawing.Size(53, 30);
+            this.btnU.TabIndex = 6;
+            this.btnU.Text = "U";
+            this.btnU.UseVisualStyleBackColor = true;
+            this.btnU.Click += new System.EventHandler(this.btnU_Click);
+            // 
+            // btnL
+            // 
+            this.btnL.Location = new System.Drawing.Point(831, 244);
+            this.btnL.Name = "btnL";
+            this.btnL.Size = new System.Drawing.Size(58, 30);
+            this.btnL.TabIndex = 7;
+            this.btnL.Text = "L";
+            this.btnL.UseVisualStyleBackColor = true;
+            this.btnL.Click += new System.EventHandler(this.btnL_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1266, 535);
+            this.Controls.Add(this.btnL);
+            this.Controls.Add(this.btnU);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnLU);
+            this.Controls.Add(this.btnMatrix);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.tbSize);
+            this.Controls.Add(this.dgMatrix);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgMatrix)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgMatrix;
+        private System.Windows.Forms.TextBox tbSize;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnMatrix;
+        private System.Windows.Forms.Button btnLU;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnU;
+        private System.Windows.Forms.Button btnL;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        int MatrixSize = 0;//розмір матриці
+        double[,] matrix;//вхідна матриця
+        double[,] L;//нижня трикутна матриця
+        double[,] U;//верхня трикутна матриця
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void ReadMatrixFromGrid()
+        {
+            matrix = new double[MatrixSize, MatrixSize + 1];//создаем матрицу
+
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                for (int j = 0; j < MatrixSize + 1; j++)
+                {
+                    matrix[i, j] = Convert.ToDouble(dgMatrix.Rows[i].Cells[j].Value);//запоминаем значения
+                }
+            }
+
+
+            ShowMatrixInGrid();//показываем матрицу
+
+        }
+
+
+        void ShowMatrixInGrid()
+        {
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                for (int j = 0; j < MatrixSize + 1; j++)
+                {
+                    dgMatrix.Rows[i].Cells[j].Value = matrix[i, j].ToString();//выводим матрицу
+                }
+            }
+        }
+
+        void CreateMatrixTest()
+        {
+            MatrixSize = Convert.ToInt32(tbSize.Text);
+            dgMatrix.Rows.Clear();
+            dgMatrix.Columns.Clear(); // очищаем солонки
+            int i = 0;
+            for (i = 0; i < MatrixSize; i++)
+            {
+                dgMatrix.Columns.Add("x" + i.ToString(), "x" + i.ToString());
+            }
+
+            dgMatrix.Columns.Add("R", "R");
+
+            dgMatrix.Rows.Add(MatrixSize);
+
+            i = 0;
+            dgMatrix.Rows[i].Cells[0].Value = "3,51";
+            dgMatrix.Rows[i].Cells[1].Value = "0,17";
+            dgMatrix.Rows[i].Cells[2].Value = "3,75";
+            dgMatrix.Rows[i].Cells[3].Value = "-0,28";
+            dgMatrix.Rows[i].Cells[4].Value = "0,75";
+
+            i = 1;
+            dgMatrix.Rows[i].Cells[0].Value = "4,52";
+            dgMatrix.Rows[i].Cells[1].Value = "2,11";
+            dgMatrix.Rows[i].Cells[2].Value = "-0,11";
+            dgMatrix.Rows[i].Cells[3].Value = "0,12";
+            dgMatrix.Rows[i].Cells[4].Value = "1,11";
+
+            i = 2;
+            dgMatrix.Rows[i].Cells[0].Value = "-2,11";
+            dgMatrix.Rows[i].Cells[1].Value = "3,17";
+            dgMatrix.Rows[i].Cells[2].Value = "0,12";
+            dgMatrix.Rows[i].Cells[3].Value = "-0,15";
+            dgMatrix.Rows[i].Cells[4].Value = "0,21";
+
+            i = 3;
+            dgMatrix.Rows[i].Cells[0].Value = "3,17";
+            dgMatrix.Rows[i].Cells[1].Value = "1,81";
+            dgMatrix.Rows[i].Cells[2].Value = "-3,17";
+            dgMatrix.Rows[i].Cells[3].Value = "0,22";
+            dgMatrix.Rows[i].Cells[4].Value = "0,05";
+
+
+        }
+
+        void CreateMatrix2()
+        {
+            MatrixSize = Convert.ToInt32(tbSize.Text);
+            dgMatrix.Rows.Clear();
+            dgMatrix.Columns.Clear(); // очищаем солонки
+            //int i = 0;
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                dgMatrix.Columns.Add("x" + i.ToString(), "x" + i.ToString());
+            }
+
+            dgMatrix.Columns.Add("R", "R");
+
+            dgMatrix.Rows.Add(MatrixSize);
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                for (int j = 0; j < MatrixSize + 1; j++)
+                {
+                    dgMatrix.Rows[i].Cells[j].Value = "1"; // записываем значеня в соответсвующие места
+                }
+            }
+        }
+
+        void CreateMatrixL()//функція яка виводить матрицю L
+        {
+            MatrixSize = Convert.ToInt32(tbSize.Text);
+            dgMatrix.Rows.Clear();
+            dgMatrix.Columns.Clear(); // очищаем солонки
+            //int i = 0;
+            //LU();
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                dgMatrix.Columns.Add("x" + i.ToString(), "x" + i.ToString());
+            }
+
+
+            dgMatrix.Rows.Add(MatrixSize);
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                for (int j = 0; j < MatrixSize; j++)
+                {
+                    dgMatrix.Rows[i].Cells[j].Value = L[i,j]; // записываем значеня в соответсвующие места
+                }
+            }
+        }
+
+        void CreateMatrixU()//функція яка виводить матрицю U
+        {
+            MatrixSize = Convert.ToInt32(tbSize.Text);
+            dgMatrix.Rows.Clear();
+            dgMatrix.Columns.Clear(); // очищаем солонки
+            //int i = 0;
+            //LU();
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                dgMatrix.Columns.Add("x" + i.ToString(), "x" + i.ToString());
+            }
+
+
+            dgMatrix.Rows.Add(MatrixSize);
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                for (int j = 0; j < MatrixSize; j++)
+                {
+                    dgMatrix.Rows[i].Cells[j].Value = U[i, j]; // записываем значеня в соответсвующие места
+                }
+            }
+        }
+        void LU()
+        {
+            matrix = new double[MatrixSize, MatrixSize + 1];
+            ReadMatrixFromGrid();
+            double[] b = new double[MatrixSize];
+            L = new double[MatrixSize, MatrixSize];
+            U = new double[MatrixSize, MatrixSize];
+            double[] y = new double[MatrixSize];
+            double[] x = new double[MatrixSize];
+            double S;
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                b[i] = matrix[i, MatrixSize];//заповнюю масив коефіцієнтами вільних членів
+            }
+
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                for (int j = 0; j < MatrixSize; j++)
+                {
+                    if (i >= j)
+                    {
+                        L[i, j] = matrix[i, j];//заповнюю значення матриці L
+                    }
+                    else
+                    {
+                        L[i, j] = 0;
+                    }
+
+                    if (i < j)
+                    {
+                        U[i, j] = matrix[i, j];//заповнюю значення матриці U
+                    }
+                    else
+                    {
+                        if (i == j)
+                        {
+                            U[i, j] = 1;
+                        }
+                        else
+                        {
+                            U[i, j] = 0;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                for (int j = 0; j < MatrixSize; j++)
+                {
+                    S = 0;
+                    if (i >= j)
+                    {
+                        for(int k = 0; k<j; k++)//-1
+                        {
+                            S = S + L[i, k] * U[k, j];
+                        }
+                        L[i, j] = matrix[i, j] - S;//рахую значення матриці L по формулам
+                    }
+                    else
+                    {
+                        for (int k = 0; k < i ; k++)
+                        {
+                            S = S + L[i, k] * U[k, j];
+                        }
+                        U[i, j] = (matrix[i, j] - S) / L[i, i]; //рахую значення матриці U по формулам
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                S = 0;
+                for (int j = 0; j < i ; j++)//-1
+                {
+                    S = S + L[i, j] * y[j];
+                }
+                y[i] = (b[i] - S) / L[i, i];
+            }
+
+            x[MatrixSize - 1] = y[MatrixSize - 1];
+            for (int i = MatrixSize - 2; i >= 0; i--) //////////
+            {
+                S = 0;
+                for(int j = i+1; j<MatrixSize; j++)
+                {
+                    S = S + U[i, j] * x[j];
+                }
+                x[i] = y[i] - S;
+            }
+
+            for(int i = 0; i <MatrixSize; i++)
+            {
+                listBox1.Items.Add("x" + i.ToString() + " = " + Math.Round(x[i],4));
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            CreateMatrixTest();
+        }
+
+        private void btnMatrix_Click(object sender, EventArgs e)
+        {
+            CreateMatrix2();
+        }
+
+        private void btnLU_Click(object sender, EventArgs e)
+        {
+            LU();
+        }
+
+        private void btnL_Click(object sender, EventArgs e)
+        {
+            CreateMatrixL();
+        }
+
+        private void btnU_Click(object sender, EventArgs e)
+        {
+            CreateMatrixU();
+        }
+    }
+}
